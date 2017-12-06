7bb2827732bf2e055c98e861310c9511560d160c
Jiri (George) Lebl <jiri.lebl@gmail.com>
2017-12-05 22:24:53 -0600

    Move figures to separate subdirectory

--- publish.sh
+++ publish.sh
@@ -11,6 +11,3 @@ makeindex scv
 pdflatex scv
 pdflatex scv
 pdflatex scv
-#scp scv.pdf lebl@fourier.math.okstate.edu:public_html/scv-s14/
-./make-tar.sh
-#scp scv.pdf scv.tar.gz jirka@zinc.5z.com:/var/www/jirka/scv/
