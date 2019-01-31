f700b19647015d7c08265d87f6680502715cfcec
Jiri (George) Lebl <jiri.lebl@gmail.com>
2019-01-30 19:13:42 -0600

    add simple environment for figures

--- getstats.sh
+++ getstats.sh
@@ -4,6 +4,8 @@ echo
 echo Chapters:"                     \t"`grep '^[\]chapter\**{' *.tex | wc -l`
 echo Sections:"                     \t"`grep '^[\]section\**{' *.tex | wc -l`
 echo
+echo Figures:"                      \t"`grep '^[\]begin{myfig' *.tex | wc -l`
+echo
 echo Exercises:"                    \t"`grep '^[\]begin{exercise' *.tex | wc -l`
 
 echo
