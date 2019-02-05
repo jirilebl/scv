a5956d409318e45ad2d781c598555bcead2db918
Jiri (George) Lebl <jiri.lebl@gmail.com>
2019-02-05 15:35:25 -0600

    minor improvement in stats (count appendices)

--- getstats.sh
+++ getstats.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 echo
-echo Chapters:"                     \t"`grep '^[\]chapter\**{' *.tex | wc -l`
-echo Sections:"                     \t"`grep '^[\]section\**{' *.tex | wc -l`
+echo "Chapters (including intro):   \t"`grep '^[\]chapter\**{.*label{ch:' *.tex | wc -l`
+echo "Appendices:                   \t"`grep '^[\]chapter{.*label{ap:' *.tex | wc -l`
+echo "Sections:                     \t"`grep '^[\]section\**{' *.tex | wc -l`
 echo
-echo Figures:"                      \t"`grep '^[\]begin{myfig' *.tex | wc -l`
+echo "Figures:                      \t"`grep '^[\]begin{myfig' *.tex | wc -l`
 echo
-echo Exercises:"                    \t"`grep '^[\]begin{exercise' *.tex | wc -l`
+echo "Exercises:                    \t"`grep '^[\]begin{exercise' *.tex | wc -l`
 
 echo
