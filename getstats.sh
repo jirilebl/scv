5c358658e75fabb50a2522c2cafe4e19c38a7403
Jiri (George) Lebl <jiri.lebl@gmail.com>
2025-05-20 22:50:39 -0500

    Fixup getstats

--- getstats.sh
+++ getstats.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 echo
-echo "Chapters (including intro):   \t"`grep '^[\]chapter\**{.*label{ch:' *.tex | wc -l`
-echo "Appendices:                   \t"`grep '^[\]chapter{.*label{ap:' *.tex | wc -l`
-echo "Sections:                     \t"`grep '^[\]section\**{' *.tex | wc -l`
+echo -e "Chapters (including intro):   \t"`grep '^[\]chapter\**{.*label{ch:' *.tex | wc -l`
+echo -e "Appendices:                   \t"`grep '^[\]chapter{.*label{ap:' *.tex | wc -l`
+echo -e "Sections:                     \t"`grep '^[\]section\**{' *.tex | wc -l`
 echo
-echo "Figures:                      \t"`grep '^[\]begin{myfig' *.tex | wc -l`
+echo -e "Figures:                      \t"`grep '^[\]begin{myfig' *.tex | wc -l`
 echo
-echo "Exercises:                    \t"`grep '^[\]begin{exercise' *.tex | wc -l`
+echo -e "Exercises:                    \t"`grep '^[\]begin{exercise' *.tex | wc -l`
 echo
-echo "Footnotes:                    \t"`grep '[\]footnote{' *.tex | wc -l`
+echo -e "Footnotes:                    \t"`grep '[\]footnote{' *.tex | wc -l`
 
 echo
